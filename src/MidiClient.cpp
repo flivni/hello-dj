beff13ddc8fed8d7a355ad65021883b16be5cc8e
Felix Livni <flivni@gmail.com>
2024-04-22 01:41:15 -0700

    Add code to handle MIDI.

--- src/MidiClient.cpp
+++ src/MidiClient.cpp
@@ -7,9 +7,13 @@
 #include "usb/usb_host.h"
 
 const char* MidiClient::TAG = "HelloDJ";
+// usb_host_client_handle_t MidiClient::Client_Handle = {0};
+// usb_device_handle_t MidiClient::Device_Handle = {0};
 
 MidiClient::MidiClient() {
-    // Constructor
+    _isMidiInterfaceClaimed = false;
+    _isMidiEndpointsPrepared = false;
+    MIDIIn[MIDI_IN_BUFFERS] = {NULL};
 }
 
 MidiClient::~MidiClient() {
@@ -45,27 +49,31 @@ void MidiClient::daemonTask(void *arg)
     ESP_ERROR_CHECK(usb_host_client_register(&client_config, &driver_obj.client_hdl));
     pThis->driver_obj = &driver_obj;
 
+    // FDL: This does not look thread safe to me.
     while (1) {
         if (driver_obj.actions == 0) {
             usb_host_client_handle_events(driver_obj.client_hdl, portMAX_DELAY);
         } else {
             if (driver_obj.actions & ACTION_OPEN_DEV) {
-                action_open_dev(&driver_obj);
+                pThis->action_open_dev(&driver_obj);
             }
             if (driver_obj.actions & ACTION_GET_DEV_INFO) {
-                action_get_info(&driver_obj);
+                pThis->action_get_info(&driver_obj);
             }
             if (driver_obj.actions & ACTION_GET_DEV_DESC) {
-                action_get_dev_desc(&driver_obj);
+                pThis->action_get_dev_desc(&driver_obj);
             }
             if (driver_obj.actions & ACTION_GET_CONFIG_DESC) {
-                action_get_config_desc(&driver_obj);
+                pThis->action_get_config_desc(&driver_obj);
+            }
+            if (driver_obj.actions & ACTION_ENABLE_MIDI) {
+                pThis->action_enable_midi(&driver_obj);
             }
             if (driver_obj.actions & ACTION_GET_STR_DESC) {
-                action_get_str_desc(&driver_obj);
+                pThis->action_get_str_desc(&driver_obj);
             }
             if (driver_obj.actions & ACTION_CLOSE_DEV) {
-                action_close_dev(&driver_obj);
+                pThis->action_close_dev(&driver_obj);
             }
             if (driver_obj.actions & ACTION_EXIT) {
                 break;
@@ -86,6 +94,7 @@ void MidiClient::client_event_cb(const usb_host_client_event_msg_t *event_msg, v
     class_driver_t *driver_obj = (class_driver_t *)arg;
     switch (event_msg->event) {
     case USB_HOST_CLIENT_EVENT_NEW_DEV:
+        // A new device has been enumerated and added to the USB Host Library
         if (driver_obj->dev_addr == 0) {
             driver_obj->dev_addr = event_msg->new_dev.address;
             //Open the device next
@@ -102,6 +111,30 @@ void MidiClient::client_event_cb(const usb_host_client_event_msg_t *event_msg, v
         //Should never occur
         abort();
     }
+}
+
+void MidiClient::midi_transfer_cb(usb_transfer_t *transfer)
+{
+    class_driver_t* driver_obj = (class_driver_t*)transfer->context;
+  ESP_LOGE("", "midi_transfer_cb."); // Was ESP_LOGI
+  if (driver_obj->dev_hdl == transfer->device_handle) {
+    int in_xfer = transfer->bEndpointAddress & USB_B_ENDPOINT_ADDRESS_EP_DIR_MASK;
+    if ((transfer->status == 0) && in_xfer) {
+      uint8_t *const p = transfer->data_buffer;
+      for (int i = 0; i < transfer->actual_num_bytes; i += 4) {
+        if ((p[i] + p[i+1] + p[i+2] + p[i+3]) == 0) break;
+        ESP_LOGE("", "midi: %02x %02x %02x %02x", // Was ESP_LOGI
+            p[i], p[i+1], p[i+2], p[i+3]);
+      }
+      esp_err_t err = usb_host_transfer_submit(transfer);
+      if (err != ESP_OK) {
+        ESP_LOGE("", "usb_host_transfer_submit In fail: %x", err); // Was ESP_LOGI
+      }
+    }
+    else {
+      ESP_LOGE("", "transfer->status %d", transfer->status); // Was ESP_LOGI
+    }
+  }
 }
 
 void MidiClient::action_open_dev(class_driver_t *driver_obj)
@@ -142,16 +175,142 @@ void MidiClient::action_get_dev_desc(class_driver_t *driver_obj)
     driver_obj->actions |= ACTION_GET_CONFIG_DESC;
 }
 
+void MidiClient::action_enable_midi(class_driver_t *driver_obj) {
+    assert(driver_obj->dev_hdl != NULL);
+    ESP_LOGE(TAG, "Detecting MIDI"); // Was LOGI
+    const usb_config_desc_t* config_desc;
+    ESP_ERROR_CHECK(usb_host_get_active_config_descriptor(driver_obj->dev_hdl, &config_desc));
+
+    const uint8_t *p = &config_desc->val[0];
+    uint8_t bLength;
+    for (int i = 0; i < config_desc->wTotalLength; i+=bLength, p+=bLength) {
+        bLength = *p;
+        if ((i + bLength) <= config_desc->wTotalLength) {
+            const uint8_t bDescriptorType = *(p + 1);
+            switch (bDescriptorType) {
+                case USB_B_DESCRIPTOR_TYPE_DEVICE:
+                    ESP_LOGE("", "USB Device Descriptor should not appear in config"); // Was LOGI
+                    break;
+                case USB_B_DESCRIPTOR_TYPE_CONFIGURATION:
+                    // show_config_desc(p);
+                    break;
+                case USB_B_DESCRIPTOR_TYPE_STRING:
+                    ESP_LOGE("", "USB string desc TBD"); // Was LOGI
+                    break;
+                case USB_B_DESCRIPTOR_TYPE_INTERFACE:
+                    // show_interface_desc(p);
+                    claimInterface(driver_obj, (usb_intf_desc_t *)p);
+                    break;
+                case USB_B_DESCRIPTOR_TYPE_ENDPOINT:
+                    // show_endpoint_desc(p);
+                    prepareEndpoints(driver_obj, (usb_ep_desc_t *)p);
+                    break;
+                case USB_B_DESCRIPTOR_TYPE_DEVICE_QUALIFIER:
+                    // Should not be in config?
+                    ESP_LOGE("", "USB device qual desc TBD"); // Was LOGI
+                    break;
+                case USB_B_DESCRIPTOR_TYPE_OTHER_SPEED_CONFIGURATION:
+                    // Should not be in config?
+                    ESP_LOGE("", "USB Other Speed TBD"); // Was LOGI
+                    break;
+                case USB_B_DESCRIPTOR_TYPE_INTERFACE_POWER:
+                    // Should not be in config?
+                    ESP_LOGE("", "USB Interface Power TBD"); // Was LOGI
+                    break;
+                default:
+                    ESP_LOGE("", "Unknown USB Descriptor Type: 0x%x", *p); // Was LOGI
+                    break;
+            }
+        } else {
+            ESP_LOGE("", "USB Descriptor invalid"); // Was ESP_LOGI
+            break;
+        }
+    }
+
+    //Get the device's string descriptors next
+    driver_obj->actions &= ~ACTION_ENABLE_MIDI;
+    driver_obj->actions |= ACTION_GET_STR_DESC;
+}
+
+void MidiClient::claimInterface(class_driver_t* driver_obj, const usb_intf_desc_t *intf)
+{
+    if (_isMidiInterfaceClaimed) return;
+
+    if ((intf->bInterfaceClass == USB_CLASS_AUDIO) &&
+        (intf->bInterfaceSubClass == 3) &&
+        (intf->bInterfaceProtocol == 0))
+    {
+        _isMidiInterfaceClaimed = true;
+        ESP_LOGE("", "Claiming a MIDI device!"); // Was ESP_LOGI
+        esp_err_t err = usb_host_interface_claim(driver_obj->client_hdl, driver_obj->dev_hdl,
+            intf->bInterfaceNumber, intf->bAlternateSetting);
+        if (err != ESP_OK) ESP_LOGE("", "usb_host_interface_claim failed: %x", err); // Was ESP_LOGI
+    }
+}
+
+void MidiClient::prepareEndpoints(class_driver_t* driver_obj, const usb_ep_desc_t* endpoint) {
+    if (!_isMidiInterfaceClaimed) {
+        ESP_LOGE("", "Unexpected: Ready to prepare endpoints, but MIDI interface not yet claimed.");
+        return;
+    }
+    if (_isMidiEndpointsPrepared) {
+        ESP_LOGE("", "Unexpected: Ready to prepare endpoints, but enpoints already prepared.");
+        return;
+    }
+
+    esp_err_t err;
+
+    // must be bulk for MIDI
+    if ((endpoint->bmAttributes & USB_BM_ATTRIBUTES_XFERTYPE_MASK) != USB_BM_ATTRIBUTES_XFER_BULK) {
+        ESP_LOGE("", "Not bulk endpoint: 0x%02x", endpoint->bmAttributes); // Was ESP_LOGI
+        return;
+    }
+    if (endpoint->bEndpointAddress & USB_B_ENDPOINT_ADDRESS_EP_DIR_MASK) {
+        for (int i = 0; i < MIDI_IN_BUFFERS; i++) {
+            err = usb_host_transfer_alloc(endpoint->wMaxPacketSize, 0, &MIDIIn[i]);
+            if (err != ESP_OK) {
+                MIDIIn[i] = NULL;
+                ESP_LOGE("", "usb_host_transfer_alloc In fail: %x", err); // Was ESP_LOGI
+            } else {
+                MIDIIn[i]->device_handle = driver_obj->dev_hdl;
+                MIDIIn[i]->bEndpointAddress = endpoint->bEndpointAddress;
+                MIDIIn[i]->callback = midi_transfer_cb;
+                MIDIIn[i]->context = driver_obj;
+                MIDIIn[i]->num_bytes = endpoint->wMaxPacketSize;
+                esp_err_t err = usb_host_transfer_submit(MIDIIn[i]);
+                if (err != ESP_OK) {
+                    ESP_LOGE("", "usb_host_transfer_submit In fail: %x", err); // Was ESP_LOGI
+                }
+            }
+        }
+    } else {
+        err = usb_host_transfer_alloc(endpoint->wMaxPacketSize, 0, &MIDIOut);
+        if (err != ESP_OK) {
+            MIDIOut = NULL;
+            ESP_LOGE("", "usb_host_transfer_alloc Out fail: %x", err); // Was ESP_LOGI
+            return;
+        }
+        ESP_LOGE("", "Out data_buffer_size: %d", MIDIOut->data_buffer_size); // Was ESP_LOGI
+        MIDIOut->device_handle = driver_obj->dev_hdl;
+        MIDIOut->bEndpointAddress = endpoint->bEndpointAddress;
+        MIDIOut->callback = midi_transfer_cb;
+        MIDIOut->context = driver_obj;
+        //    MIDIOut->flags |= USB_TRANSFER_FLAG_ZERO_PACK;
+    }
+    _isMidiEndpointsPrepared = ((MIDIOut != NULL) && (MIDIIn[0] != NULL));
+}
+
 void MidiClient::action_get_config_desc(class_driver_t *driver_obj)
 {
     assert(driver_obj->dev_hdl != NULL);
     ESP_LOGE(TAG, "Getting config descriptor"); // Was LOGI
-    const usb_config_desc_t *config_desc;
+    const usb_config_desc_t* config_desc;
     ESP_ERROR_CHECK(usb_host_get_active_config_descriptor(driver_obj->dev_hdl, &config_desc));
     usb_print_config_descriptor(config_desc, NULL);
+
     //Get the device's string descriptors next
     driver_obj->actions &= ~ACTION_GET_CONFIG_DESC;
-    driver_obj->actions |= ACTION_GET_STR_DESC;
+    driver_obj->actions |= ACTION_ENABLE_MIDI;
 }
 
 void MidiClient::action_get_str_desc(class_driver_t *driver_obj)
--- src/MidiClient.h
+++ src/MidiClient.h
@@ -6,6 +6,13 @@
 #include "esp_log.h"
 #include "usb/usb_host.h"
 
+typedef struct {
+    usb_host_client_handle_t client_hdl;
+    uint8_t dev_addr;
+    usb_device_handle_t dev_hdl;
+    uint32_t actions;
+} class_driver_t;
+
 class MidiClient {
     public: 
         const UBaseType_t DAEMON_TASK_PRIORITY = 3;
@@ -30,18 +37,15 @@ class MidiClient {
             ACTION_GET_DEV_INFO = 0x02,
             ACTION_GET_DEV_DESC = 0x04,
             ACTION_GET_CONFIG_DESC = 0x08,
-            ACTION_GET_STR_DESC = 0x10,
-            ACTION_CLOSE_DEV = 0x20,
-            ACTION_EXIT = 0x40,
-            ACTION_RECONNECT = 0x80,
+            ACTION_ENABLE_MIDI = 0x10,
+            ACTION_GET_STR_DESC = 0x20,
+            ACTION_CLOSE_DEV = 0x40,
+            ACTION_EXIT = 0x80,
+            ACTION_RECONNECT = 0x100,
         } action_t;
 
-        typedef struct {
-            usb_host_client_handle_t client_hdl;
-            uint8_t dev_addr;
-            usb_device_handle_t dev_hdl;
-            uint32_t actions;
-        } class_driver_t;
+        // static usb_host_client_handle_t Client_Handle;
+        static usb_device_handle_t Device_Handle;
 
         class_driver_t* driver_obj;
 
@@ -49,12 +53,25 @@ class MidiClient {
 
         TaskHandle_t _class_driver_task_hdl;
 
+        bool _isMidiInterfaceClaimed;
+        bool _isMidiEndpointsPrepared;
+
+        static const size_t MIDI_IN_BUFFERS = 8;
+        static const size_t MIDI_OUT_BUFFERS = 8;
+        usb_transfer_t *MIDIOut = NULL;
+        usb_transfer_t* MIDIIn[MIDI_IN_BUFFERS] = {NULL};
+
         static void daemonTask(void* arg);
         static void client_event_cb(const usb_host_client_event_msg_t *event_msg, void *arg);
-        static void action_open_dev(class_driver_t *driver_obj);
-        static void action_get_info(class_driver_t *driver_obj);
-        static void action_get_dev_desc(class_driver_t *driver_obj);
-        static void action_get_config_desc(class_driver_t *driver_obj);
-        static void action_get_str_desc(class_driver_t *driver_obj);
-        static void action_close_dev(class_driver_t *driver_obj);
+        static void midi_transfer_cb(usb_transfer_t *transfer);
+
+        void action_open_dev(class_driver_t *driver_obj);
+        void action_get_info(class_driver_t *driver_obj);
+        void action_get_dev_desc(class_driver_t *driver_obj);
+        void action_get_config_desc(class_driver_t *driver_obj);
+        void action_get_str_desc(class_driver_t *driver_obj);
+        void action_close_dev(class_driver_t *driver_obj);
+        void action_enable_midi(class_driver_t* driver_obj);
+        void claimInterface(class_driver_t* driver_obj, const usb_intf_desc_t* intf);
+        void prepareEndpoints(class_driver_t* driver_obj, const usb_ep_desc_t* endpoint);
 };
